5b54674ee729d19dc3c631e3688247cc4de94752
perezc99 <165786162+perezc99@users.noreply.github.com>
2024-04-25 16:49:43 -0700

    Finished Mock Portfolio

--- APP_2_Portfolio/send_email.py
+++ APP_2_Portfolio/send_email.py
@@ -1,4 +1,5 @@
 import smtplib, ssl
+import os
 
 
 def send_email(message):
@@ -6,7 +7,7 @@ def send_email(message):
     port = 465
 
     username = "bus.ces.per@gmail.com"
-    password = "jghkklzqetyleury"
+    password = os.getenv("BUSCESPERPWD")
 
     receiver = username
     context = ssl.create_default_context()
